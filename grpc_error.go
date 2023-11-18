2f6c7964f1e2b775134052ab9bcb1941f6cbe741
leoncolt <lionmanbloodsucker@yahoo.com>
2023-11-18 13:27:01 +0700

    replace GrpcException with Error
    Signed-off-by: leoncolt <lionmanbloodsucker@yahoo.com>

--- grpc_error.go
+++ grpc_error.go
@@ -1,42 +1,57 @@
 package ez_grpc
 
 import (
-	"fmt"
-
+	"github.com/LeonColt/ez"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
 )
 
-type GrpcException interface {
-	GetGrpcCode() codes.Code
-}
-
-type GrpcErrorBuilder struct {
-	GrpcCode codes.Code
-	Message  string
-}
-
-func (ptr *GrpcErrorBuilder) GetGrpcCode() codes.Code { return ptr.GrpcCode }
-
-func (ptr *GrpcErrorBuilder) Error() string { return ptr.Message }
-
-type GrpcErrorBuilderWithError struct {
-	GrpcErrorBuilder
-	Err error
-}
-
-func (ptr *GrpcErrorBuilderWithError) GetGrpcCode() codes.Code { return ptr.GrpcCode }
-
-func (ptr *GrpcErrorBuilderWithError) Error() string {
-	return fmt.Sprintf("%s: %#v", ptr.Message, ptr.Err)
+func ParseErrorCodeToGrpcCode(in ez.ErrorCode) codes.Code {
+	switch in {
+	case ez.ErrorCodeOk:
+		return codes.OK
+	case ez.ErrorCodeCancelled:
+		return codes.Canceled
+	case ez.ErrorCodeUnknown:
+		return codes.Unknown
+	case ez.ErrorCodeInvalidArgument:
+		return codes.InvalidArgument
+	case ez.ErrorCodeDeadlineExceeded:
+		return codes.DeadlineExceeded
+	case ez.ErrorCodeNotFound:
+		return codes.NotFound
+	case ez.ErrorCodeConflict:
+		return codes.AlreadyExists
+	case ez.ErrorCodeNotAuthorized:
+		return codes.PermissionDenied
+	case ez.ErrorCodeResourceExhausted:
+		return codes.ResourceExhausted
+	case ez.ErrorCodeFailedPrecondition:
+		return codes.FailedPrecondition
+	case ez.ErrorCodeAborted:
+		return codes.Aborted
+	case ez.ErrorCodeOutOfRange:
+		return codes.OutOfRange
+	case ez.ErrorCodeUnimplemented:
+		return codes.Unimplemented
+	case ez.ErrorCodeInternal:
+		return codes.Internal
+	case ez.ErrorCodeUnavailable:
+		return codes.Unavailable
+	case ez.ErrorCodeDataLoss:
+		return codes.DataLoss
+	case ez.ErrorCodeUnauthenticated:
+		return codes.Unauthenticated
+	}
+	return codes.Unknown
 }
 
 func HandleGrpcError(err error) error {
 	if err == nil {
 		return nil
 	}
-	if grpcErr, ok := err.(GrpcException); ok {
-		return status.Error(grpcErr.GetGrpcCode(), err.Error())
+	if grpcErr, ok := err.(*ez.Error); ok {
+		return status.Error(ParseErrorCodeToGrpcCode(grpcErr.Code), grpcErr.Error())
 	} else {
 		return status.Errorf(codes.Internal, "unknown error: %#v", err)
 	}
--- grpc_error_test.go
+++ grpc_error_test.go
@@ -3,65 +3,17 @@ package ez_grpc_test
 import (
 	"testing"
 
+	"github.com/LeonColt/ez"
 	"github.com/LeonColt/ez_grpc"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
 )
 
-func getBuilder(code codes.Code, message string) any {
-	return &ez_grpc.GrpcErrorBuilder{
-		GrpcCode: code,
-		Message:  message,
-	}
-}
-
-func TestCheckType(t *testing.T) {
-	{
-		err := getBuilder(codes.OK, "OK")
-		res, ok := err.(ez_grpc.GrpcException)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, res)
-	}
-
-	{
-		err := getBuilder(codes.NotFound, "Not Found")
-		res, ok := err.(ez_grpc.GrpcException)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, res)
-	}
-
-	{
-		err := getBuilder(codes.Internal, "Internal Server Error")
-		res, ok := err.(ez_grpc.GrpcException)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, res)
-	}
-}
-
-func TestGetCode(t *testing.T) {
-	{
-		err := getBuilder(codes.OK, "OK")
-		res, ok := err.(ez_grpc.GrpcException)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, res)
-		require.Equal(t, codes.OK, res.GetGrpcCode())
-	}
-
-	{
-		err := getBuilder(codes.NotFound, "Not Found")
-		res, ok := err.(ez_grpc.GrpcException)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, res)
-		require.Equal(t, codes.NotFound, res.GetGrpcCode())
-	}
-
-	{
-		err := getBuilder(codes.Internal, "Internal Server Error")
-		res, ok := err.(ez_grpc.GrpcException)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, res)
-		require.Equal(t, codes.Internal, res.GetGrpcCode())
+func getBuilder(code ez.ErrorCode, message string) *ez.Error {
+	return &ez.Error{
+		Code:    code,
+		Message: message,
 	}
 }
 
@@ -72,32 +24,21 @@ func TestHandleGrpcError(t *testing.T) {
 	}
 
 	{
-		ezerr := getBuilder(codes.OK, "OK")
-		err, ok := ezerr.(*ez_grpc.GrpcErrorBuilder)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, err)
-		require.Equal(t, codes.OK, err.GetGrpcCode())
+		err := getBuilder(ez.ErrorCodeOk, "OK")
+		require.Equal(t, codes.OK, ez_grpc.ParseErrorCodeToGrpcCode(err.Code))
 		grpcerr := ez_grpc.HandleGrpcError(err)
 		require.Nil(t, grpcerr)
 	}
-
 	{
-		ezerr := getBuilder(codes.NotFound, "Item was not found")
-		err, ok := ezerr.(*ez_grpc.GrpcErrorBuilder)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, err)
-		require.Equal(t, codes.NotFound, err.GetGrpcCode())
+		err := getBuilder(ez.ErrorCodeNotFound, "Item was not found")
+		require.Equal(t, codes.NotFound, ez_grpc.ParseErrorCodeToGrpcCode(err.Code))
 		grpcerr := ez_grpc.HandleGrpcError(err)
-		require.EqualError(t, grpcerr, status.Error(codes.NotFound, "Item was not found").Error())
+		require.EqualError(t, grpcerr, status.Error(codes.NotFound, "5: Item was not found").Error())
 	}
-
 	{
-		ezerr := getBuilder(codes.Internal, "error parsing")
-		err, ok := ezerr.(*ez_grpc.GrpcErrorBuilder)
-		require.True(t, ok)
-		require.IsType(t, &ez_grpc.GrpcErrorBuilder{}, err)
-		require.Equal(t, codes.Internal, err.GetGrpcCode())
+		err := getBuilder(ez.ErrorCodeInternal, "error parsing")
+		require.Equal(t, codes.Internal, ez_grpc.ParseErrorCodeToGrpcCode(err.Code))
 		grpcerr := ez_grpc.HandleGrpcError(err)
-		require.EqualError(t, grpcerr, status.Error(codes.Internal, "error parsing").Error())
+		require.EqualError(t, grpcerr, status.Error(codes.Internal, "13: error parsing").Error())
 	}
 }
